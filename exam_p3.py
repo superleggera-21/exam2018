7c7ff9deb9809142807fed20992a2e03679d9cfe
Jingyu (Jack) He <jhe1@babson.edu>
2018-11-13 19:43:51 -0500

    Exam Submission
    This is the final submission of the exam.

--- exam_p3.py
+++ exam_p3.py
@@ -5,14 +5,18 @@ class Employee:
     idnumber = 0
     def __init__(self, name):
         self.name = name
-        self.id = (idnumber+1)
-        idnumber += 1
+        self.id = Employee.idnumber
+        Employee.idnumber += 1
+
+    def __str__(self):
+        return str(self.id)
 
     def get_name(self):
         return self.name
 
     def weekly_pay(self, hours_worked):
         return 0
+
 
 
 class Nonexempt_Employee(Employee):
@@ -29,13 +33,24 @@ class Nonexempt_Employee(Employee):
             return hours_worked * self.hourly_rate
 
 class Exempt_Employee(Employee):
+
     def __init__(self, name, annual_salary):
         Employee.__init__(self, name)
-        self.hourly_rate = hourly_rate
+        self.annual_salary = annual_salary
 
+    def weekly_pay(self, hours_worked):
+        return self.annual_salary/52
 
 class Manager(Exempt_Employee):
-    pass  # delete this line and replace with your code here
+
+    def __init__(self, name, annual_salary, bonus):
+        Exempt_Employee.__init__(self, name, annual_salary)
+        self.bonus = bonus
+
+
+    def weekly_pay(self, hours_worked):
+        return (self.annual_salary)/52 + self.bonus
+
 
 
 def main():
@@ -44,10 +59,11 @@ def main():
     all_employees.append(Exempt_Employee("Alden Pexton", 60000.0))
     all_employees.append(Manager("Allison Fernandez", 94000.0, 50.0))
 
+
     for employee in all_employees:
         hours = int(input("Hours worked by " + employee.get_name() + ": "))
         pay = employee.weekly_pay(hours)
-        print("Salary: ", pay)
+        print("Salary of employee No." , employee, ": ", pay)
 
 
 if __name__ == '__main__':
--- exam_p4.py
+++ exam_p4.py
@@ -4,3 +4,31 @@ What is the most interesting/funny/cool thing(s) about Python that you learned f
 You can use code or a short paragraph to illustrate it.
 """
 
+# As a computer enthusiast and a worker in Babson IT, I looked for some ways that Python can help us better pull up data
+# from students / faculties' laptop. And by using some libraries such as platform, some lines of python code can help us
+# see what computers the users are using, as well as their Asset Tag ID.
+
+print("Gathering system info...")
+
+import platform
+bit = platform.machine()
+platformname = platform.platform()
+generalinfo = platform.uname()
+system = platform.system()
+CPU = platform.processor()
+
+print("The user is running ", bit[-2:], "bit system.")
+print("OS: ", platformname.split('-')[0],platformname.split('-')[1])
+print("Asset Tag / System ID: ", generalinfo[1])
+if generalinfo[1][0:2] == "L1":
+    print("This is a Babson-issued laptop.")
+    if generalinfo[1][2] == "8":
+        print("Model Number: Lenovo X1-Yoga Gen3")
+    else:
+        print("Model Number: Lenovo X1-Yoga Gen2")
+elif generalinfo[1][0:2] == "D1":
+    print("This is a Babson-issued desktop.")
+else:
+    print('This is a non-Babson computer.')
+
+print("The computer is running a", generalinfo[-1].split()[-1], "CPU.")
